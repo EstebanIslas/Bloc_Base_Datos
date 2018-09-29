07eeb5bba28b75efeb66bcdb87e1592342655c62
USUARIO <USUARIO@192.168.1.69>
2018-09-29 09:22:56 -0500

    Botones de Primero y ultimo Registro

--- src/model/ModelBlocBase.java
+++ src/model/ModelBlocBase.java
@@ -37,5 +37,34 @@ public class ModelBlocBase {
     public void setPath(String path) {
         this.path = path;
     }
+    
+    public String separarCadena(String nom, String ape){
+        String cadena = "";
+        cadena = nom + "," + ape;
+        return cadena;
+    }
+    
+    public void primerRegistro(String cadena) {
+        String separar = cadena;
+        String[] saltoLinea = separar.split("\n");
+        String[] sep_coma = saltoLinea[0].split(",");
+        this.setNombre(sep_coma[0]);
+        this.setApellido(sep_coma[1]);
+        
+        /*nombre = resul2[0];
+        apellido += resul2[1];
+        System.out.println(nombre);
+        System.out.println(apell);*/
+    } 
+    
+    public void ultimoRegistro(String cadena) {
+        String separar = cadena;
+        String[] saltoLinea = separar.split("\n");
+        int i = (saltoLinea.length - 1);
+        String[] sep_coma = saltoLinea[i].split(",");
+        this.setNombre(sep_coma[0]);
+        this.setApellido(sep_coma[1]);
+        
+    }
 
 }
